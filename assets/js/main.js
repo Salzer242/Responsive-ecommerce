864ff2ae3418fbb4754834a1c538984c25afdb63
David Wonder <wonderbakaledavid@gmail.com>
2021-08-08 05:55:49 +0100

    Suppression du menu déroulant sur mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,19 @@ const showMenu = (toggleId, navId) =>{
     }
 }
 
+showMenu('nav-toggle','nav-menu')
+
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('nav__link')
+
+function linkAction(){
+    // Active link
+    navLink.forEach(n => n.classList.remove('nav-menu'))
+    this.classList.add('active')
+
+    // Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
